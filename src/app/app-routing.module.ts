1d898ba8621ace8cc50814d39aa2c70ae13d7042
David <david.cicconi94@gmail.com>
2022-03-04 02:33:31 -0300

    create employee component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,29 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CreateEmployeesComponent } from './components/create-employees/create-employees.component';
+import { ListEmployeesComponent } from './components/list-employees/list-employees.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'list-employees',
+    pathMatch: 'full'
+  },
+  {
+    path: 'list-employees',
+    component: ListEmployeesComponent
+  },
+  {
+    path: 'create-employee',
+    component: CreateEmployeesComponent
+  },
+  {
+    path: '**',
+    redirectTo: 'list-employees',
+    pathMatch: 'full'
+  },
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
